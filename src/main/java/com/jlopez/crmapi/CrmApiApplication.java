8606536d0b02783b360faf21a5ceb7f1808e71dc
Jose Maria Lopez Diaz <KiroHertz@gmail.com>
2018-12-24 15:32:07 +0000

    Initial commit

---
+++ src/main/java/com/jlopez/crmapi/CrmApiApplication.java
@@ -0,0 +1,14 @@
+package com.jlopez.crmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jlopez/crmapi/CrmApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jlopez.crmapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrmApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
